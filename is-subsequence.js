ab5922b99e5e5f10663596e9ecf312aaca35f4c7
OGMorales17 <chorly786@gmail.com>
2021-12-22 16:54:59 -0500

    .

---
+++ is-subsequence.js
@@ -0,0 +1,24 @@
+/***************************************************************************************************
+ 
+isSubsequence
+Write a function called isSubsequence which takes in two strings and checks whether the characters 
+in the first string form a subsequence of the characters in the second string. 
+In other words, the function should check whether the characters in the first string appear 
+somewhere in the second string, without their order changing.
+
+Constraints:
+
+Time Complexity - O(N + M)
+
+add whatever parameters you deem necessary
+*****************************************************************************************************/
+
+// Examples:
+
+isSubsequence('hello', 'hello world'); // true
+isSubsequence('sing', 'sting'); // true
+isSubsequence('abc', 'abracadabra'); // true
+isSubsequence('abc', 'acb'); // false (order matters)
+
+function isSubsequence() {
+}
---
+++ longest-fall.js
@@ -0,0 +1,22 @@
+/*****************************************************************************
+ 
+Further Study
+
+longestFall
+Write a function called longestFall, which accepts an array of integers, 
+and returns the length of the longest consecutive decrease of integers.
+
+add whatever parameters you deem necessary
+*****************************************************************************/
+
+// Examples:
+
+longestFall([5, 3, 1, 3, 0]) // 3, 5-3-1 is the longest consecutive sequence of decreasing integers
+longestFall([2, 2, 1]) // 2, 2-1 is the longest consecutive sequence of decreasing integers
+longestFall([2, 2, 2]) // 1, 2 is the longest consecutive sequence of decreasing integers
+longestFall([5, 4, 4, 4, 3, 2]) // 3, 4-3-2 is the longest
+longestFall([9, 8, 7, 6, 5, 6, 4, 2, 1]) // 5, 9-8-7-6-5 is the longest
+longestFall([]) // 0
+
+function longestFall() {
+}
